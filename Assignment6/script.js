23a427818d050f59c1b6df66809cb15c242945a8
Iman A <imooniximan@gmail.com>
2025-03-21 17:04:08 -0400

    javascript file

---
+++ Assignment6/script.js
@@ -0,0 +1,12 @@
+// Display a message in the browser console
+console.log("CSS + JavaScript is powerful!");
+
+// Show an alert message when the page loads
+window.onload = function () {
+    alert("Welcome to Assignment 5 with JavaScript!");
+};
+
+// Function to toggle between light mode and dark mode
+function changeTheme() {
+    document.body.classList.toggle("dark-mode");
+}
