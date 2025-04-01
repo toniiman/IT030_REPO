00a24e217201bd75dfa8ab38d43fa7cb54fbad8c
Iman A <imooniximan@gmail.com>
2025-04-01 00:00:36 -0400

    Add close button functionality to mobile navigation

--- Midterm/script2.js
+++ Midterm/script2.js
@@ -1,8 +1,15 @@
 const bar =document.getElementById('bar');
 const nav =document.getElementById('navbar');
+const close =document.getElementById('close')
 
 if (bar) {
     bar.addEventListener('click', () => {
         nav.classList.toggle('show');
-    });
+    }); 
 }
+
+if (close) {
+    bar.addEventListener('click', () => {
+        nav.classList.remove('show');
+    }); 
+} 
